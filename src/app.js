1e3134f9a6bb801c9183fedc991d93da12582390
RikiTeethdog <flanseros@yahoo.es>
2025-01-30 11:25:48 +0000

    Hecho

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let palos = ["♦", "♥", "♠", "♣"]
+let carta = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+let color = ["red", "black"]
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function fuckingHell (array){
+    let nuevoIndice = Math.floor(Math.random() * array.length);
+    return array[nuevoIndice];
+}
+
+function cartaDef () {
+
+    let nuevoPalo = fuckingHell(palos);
+    let nuevaCarta = fuckingHell(carta);
+    let otroColor = "black"
+    if (nuevoPalo === "♦" || nuevoPalo === "♥") {
+        otroColor = "red"
+    }
+
+    document.querySelector(".cartaJugada").style.color = otroColor;
+    document.querySelector("#paloSuperior").innerHTML = nuevoPalo;
+    
+    document.querySelector("#numeroActual").innerHTML = nuevaCarta;
+    document.querySelector("#paloInferior").innerHTML = nuevoPalo;
 };
-
+window.onload=function(){
+    cartaDef ();
+}
